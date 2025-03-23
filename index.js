0de475bb8d6573b39e1bcdbde913b6776b145643
Sandeep Kumar Mehta <112331347+sandeepkrmehta@users.noreply.github.com>
2025-03-24 03:56:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const accountSid = 'AC963a808d8b2265aca9b0f340980fc691';
 const authToken = 'bda099d4a801917efb102bd022d3f894';
 const client = require('twilio')(accountSid, authToken);
 
-const phoneNumbers = ['7222976619','7089879563','7261048972','9098569052','7067612077','8349138566']
+const phoneNumbers = ['7222976699','7089879553','7261040972','9098569072','7067611077','8376138566']
 
 phoneNumbers.forEach(element => {
   client.messages
